5edb110f197201a79f90b2e6212fb090cf4027aa
Anna Valeria Makaria <94723781+OnnaMcadva@users.noreply.github.com>
2023-11-02 16:18:24 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmakaro <anmakaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 12:16:41 by anmakaro          #+#    #+#             */
+/*   Updated: 2023/11/02 12:16:42 by anmakaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -9,26 +21,26 @@
 # include <string.h>
 # include <limits.h>
 
-
 void	ft_putchar(char c);
 char	*ft_itoa(int n);
-int	ft_print_decimal_integer(int n);
-int	ft_hexlen(unsigned	int num);
+int		ft_print_decimal_integer(int n);
+int		ft_hexlen(unsigned	int num);
 void	ft_puthex(unsigned int num, const char format);
-int	ft_print_hexadecimal(unsigned int num, const char format);
-int	ft_init_format(va_list args, const char format);
-int	ft_print_percent_sign(void);
-int	ft_ptrlen(uintptr_t num);
+int		ft_print_hexadecimal(unsigned int num, const char format);
+int		ft_init_format(va_list args, const char format);
+int		ft_print_percent_sign(void);
+int		ft_ptrlen(uintptr_t num);
 void	ft_putptr(uintptr_t num);
-int	ft_print_pointer(unsigned long long ptr);
-int	ft_printf(const char *str, ...);
-int	ft_print_char(int c);
+int		ft_print_pointer(unsigned long long ptr);
+int		ft_printf(const char *str, ...);
+int		ft_print_char(int c);
 void	ft_putstr(char *str);
-int	ft_print_string(char *str);
-int	ft_numlen(unsigned	int num);
+int		ft_print_string(char *str);
+int		ft_numlen(unsigned	int num);
 char	*ft_uitoa(unsigned int n);
-int	ft_print_unsigned_decimal(unsigned int n);
+int		ft_print_unsigned_decimal(unsigned int n);
+/*
 int main();
 void test_function();
-
+*/
 #endif
